35234da3877fdd6f8449b867978e157cb3a4f8de
Adam Tilly <tillyjadam@gmail.com>
2021-09-19 13:33:50 -0500

    time block text is now editable

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,3 +3,32 @@ function displayTime() {
     $('.clock').html(time);
 }
 displayTime();
+
+// add ability to edit task info
+$(".task-text").on("click", function() {
+    // get current value of textarea
+    var text = $(this)
+    .text()
+    .trim();
+
+  // replace div element with a new textarea
+  var textInput = $("<textarea>").val(text)
+  .addClass("col-10 past task-text ");
+  $(this).replaceWith(textInput);
+
+  // auto focus new element
+  textInput.trigger("focus");
+});
+$(".task-text").on("blur", function() {
+    var text = $(this)
+    .val()
+    .trim();
+    var taskDiv = $("<div>")
+    .text(text)
+    .addClass("col-10 past task-text");
+    // replace textarea with p element
+$(".task-text").replaceWith(taskDiv);
+});
+
+
+// update time-block style based on time
