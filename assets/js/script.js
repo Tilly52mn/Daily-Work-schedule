c7fc6563d8843b156f361fd283ed443afc06ec32
Adam Tilly <tillyjadam@gmail.com>
2021-09-19 22:09:28 -0500

    added save and load functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,11 +13,14 @@ $(".task-text").on("click", function () {
 
     // replace div element with a new textarea
     var textInput = $("<textarea>").val(text)
-        .addClass("col-10 past task-text ");
+        .addClass("col-10 task-text ");
     $(this).replaceWith(textInput);
 
     // auto focus new element
     textInput.trigger("focus");
+    $(".task-text").each(function () {
+        auditTask(this);
+    });
 });
 $(".task-text").on("blur", function () {
     var text = $(this)
@@ -25,7 +28,7 @@ $(".task-text").on("blur", function () {
         .trim();
     var taskDiv = $("<div>")
         .text(text)
-        .addClass("col-10 past task-text");
+        .addClass("col-10 task-text");
     // replace textarea with p element
     $(".task-text").replaceWith(taskDiv);
 });
@@ -35,28 +38,65 @@ var auditTask = function (taskEl) {
     // // remove any old classes from element
     $(taskEl).removeClass("past present future");
     // get tme from task element
-    console.log("id of closest row = " + $(taskEl).closest('.row').attr('id'));
-    var timeBlock = $(taskEl).attr("id");
-    // // ensure it worked
-    console.log("timeBlock id = " + timeBlock);
+    var timeBlock = $(taskEl).closest('.row').attr('id');
 
-    // convert to moment object at 5:00pm
+    // convert to moment object at current hour
     var time = moment().format("H");
-    console.log(time)
-    //       // apply new class if task is near/over due date
+// apply new class if task is past/current/or future time
     if (time > timeBlock) {
         $(taskEl).addClass("past");
     }
-    else if (time === timeBlock) {
+    else if (time == timeBlock) {
         $(taskEl).addClass("present");
     }
     else if (time < timeBlock) {
         $(taskEl).addClass("future");
     }
 };
-// update time-block style based on time
+
+//save task
+$(".saveBtn").on("click", function(){
+    var id = $(this).closest('.row').attr('id');
+    var text =$(this).siblings('.task-text').val();
+   var task = localStorage.setItem(id,text);
+})
+
+//load all tasks at refresh
+ function loadPast() {
+     //get info from local storage
+  var task9am =   localStorage.getItem(9);
+  var task10am =   localStorage.getItem(10);
+  var task11am =   localStorage.getItem(11);
+  var task12pm =   localStorage.getItem(12);
+  var task1pm =   localStorage.getItem(13);
+  var task2pm =   localStorage.getItem(14);
+  var task3pm =   localStorage.getItem(15);
+  var task4pm =   localStorage.getItem(16);
+  var task5pm =   localStorage.getItem(17);
+
+//put text into correct spots
+$("#9").children('.task-text').val(task9am);
+$("#10").children('.task-text').val(task10am);
+$("#11").children('.task-text').val(task11am);
+$("#12").children('.task-text').val(task12pm);
+$("#13").children('.task-text').val(task1pm);
+$("#14").children('.task-text').val(task2pm);
+$("#15").children('.task-text').val(task3pm);
+$("#16").children('.task-text').val(task4pm);
+$("#17").children('.task-text').val(task5pm);
+}
+
+// update time-block style based on time at first opening
 setTimeout(function () {
     $(".task-text").each(function () {
         auditTask(this);
     });
-}, (1000 * 1))
+    loadPast()
+}, (1))
+
+// update time-block style based on time every minuet
+setTimeout(function () {
+    $(".task-text").each(function () {
+        auditTask(this);
+    });
+}, (1000 * 60))
